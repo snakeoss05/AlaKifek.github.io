faeb7144b454fbb199ccd9b27215761d50bc93e1
snakeoss <bibani.noussaier@gmail.com>
2023-04-30 17:09:22 +0200

    update

--- Frontend/src/pages/log.tsx
+++ Frontend/src/pages/log.tsx
@@ -98,139 +98,146 @@ export default function Log() {
     setlogin(!login);
   }
   return (
-    <div
-      className="d-flex justify-content-center align-items-center flex-column familyMon h-100vh py-5"
-      id="logform"
-      style={{ backgroundColor: "#f6f5f7" }}
-    >
-      <>
+    <>
+      {isLoggedIn ? (
+        <UserAccount />
+      ) : (
         <div
-          className={`container ${login ? "right-panel-active" : ""}`}
-          id="container"
+          className="d-flex justify-content-center align-items-center flex-column familyMon h-100vh py-5"
+          id="logform"
+          style={{ backgroundColor: "#f6f5f7" }}
         >
-          <div className="form-container sign-up-container">
-            <form action="#" onSubmit={registerform}>
-              <h1>Create Account</h1>
-              <div className="social-container">
-                <a href="#" className="social">
-                  <i className="fab fa-facebook-f" />
-                </a>
-                <a href="#" className="social">
-                  <i className="fab fa-google-plus-g" />
-                </a>
-                <a href="#" className="social">
-                  <i className="fab fa-linkedin-in" />
-                </a>
+          <>
+            <div
+              className={`container ${login ? "right-panel-active" : ""}`}
+              id="container"
+            >
+              <div className="form-container sign-up-container">
+                <form action="#" onSubmit={registerform}>
+                  <h1>Create Account</h1>
+                  <div className="social-container">
+                    <a href="#" className="social">
+                      <i className="fab fa-facebook-f" />
+                    </a>
+                    <a href="#" className="social">
+                      <i className="fab fa-google-plus-g" />
+                    </a>
+                    <a href="#" className="social">
+                      <i className="fab fa-linkedin-in" />
+                    </a>
+                  </div>
+                  <span>or use your email for registration</span>
+                  <input
+                    type="text"
+                    name="username"
+                    placeholder="username"
+                    onChange={HandleChange}
+                    value={register.username}
+                  />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    onChange={HandleChange}
+                    value={register.email}
+                  />
+                  <input
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                    onChange={HandleChange}
+                    value={register.password}
+                  />
+                  {signinmsg && (
+                    <div
+                      className="alert success-danger mx-auto"
+                      style={{ fontSize: "14px" }}
+                      role="alert"
+                    >
+                      {signinmsg}
+                    </div>
+                  )}
+                  {verificationMessage && (
+                    <div
+                      className="alert alert-danger  mx-auto"
+                      style={{ fontSize: "14px" }}
+                      role="alert"
+                    >
+                      {verificationMessage}
+                    </div>
+                  )}
+                  <button type="submit">Sign Up</button>
+                </form>
               </div>
-              <span>or use your email for registration</span>
-              <input
-                type="text"
-                name="username"
-                placeholder="username"
-                onChange={HandleChange}
-                value={register.username}
-              />
-              <input
-                type="email"
-                name="email"
-                placeholder="email"
-                onChange={HandleChange}
-                value={register.email}
-              />
-              <input
-                type="password"
-                placeholder="password"
-                name="password"
-                onChange={HandleChange}
-                value={register.password}
-              />
-              {signinmsg && (
-                <div
-                  className="alert success-danger mx-auto"
-                  style={{ fontSize: "14px" }}
-                  role="alert"
-                >
-                  {signinmsg}
-                </div>
-              )}
-              {verificationMessage && (
-                <div
-                  className="alert alert-danger  mx-auto"
-                  style={{ fontSize: "14px" }}
-                  role="alert"
-                >
-                  {verificationMessage}
-                </div>
-              )}
-              <button type="submit">Sign Up</button>
-            </form>
-          </div>
-          <div className="form-container sign-in-container">
-            <form onSubmit={loginform}>
-              <h1>Sign in</h1>
-              <div className="social-container">
-                <a href="#" className="social">
-                  <i className="fab fa-facebook-f" />
-                </a>
-                <a href="#" className="social">
-                  <i className="fab fa-google-plus-g" />
-                </a>
-                <a href="#" className="social">
-                  <i className="fab fa-linkedin-in" />
-                </a>
-              </div>
-              <span>or use your account</span>
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                onChange={HandleChange}
-                value={logform.email}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                onChange={HandleChange}
-                name="password"
-                value={logform.password}
-              />
-              <a href="#">Forgot your password?</a>
-              {verificationMessaget && (
-                <div
-                  className={`alert alert-danger  mx-auto  alert-verification ${verificationMessaget &&
-                    "alertfadeup"}`}
-                  style={{ fontSize: "14px" }}
-                  role="alert"
-                >
-                  {verificationMessaget}
-                </div>
-              )}
+              <div className="form-container sign-in-container">
+                <form onSubmit={loginform}>
+                  <h1>Sign in</h1>
+                  <div className="social-container">
+                    <a href="#" className="social">
+                      <i className="fab fa-facebook-f" />
+                    </a>
+                    <a href="#" className="social">
+                      <i className="fab fa-google-plus-g" />
+                    </a>
+                    <a href="#" className="social">
+                      <i className="fab fa-linkedin-in" />
+                    </a>
+                  </div>
+                  <span>or use your account</span>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    onChange={HandleChange}
+                    value={logform.email}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    onChange={HandleChange}
+                    name="password"
+                    value={logform.password}
+                  />
+                  <a href="#">Forgot your password?</a>
+                  {verificationMessaget && (
+                    <div
+                      className={`alert alert-danger  mx-auto  alert-verification ${verificationMessaget &&
+                        "alertfadeup"}`}
+                      style={{ fontSize: "14px" }}
+                      role="alert"
+                    >
+                      {verificationMessaget}
+                    </div>
+                  )}
 
-              <button>Sign In</button>
-            </form>
-          </div>
-          <div className="overlay-container">
-            <div className="overlay">
-              <div className="overlay-panel overlay-left">
-                <h1>Welcome Back!</h1>
-                <p>
-                  To keep connected with us please login with your personal info
-                </p>
-                <button className="ghost" id="signIn" onClick={Changestyle}>
-                  Sign In
-                </button>
+                  <button>Sign In</button>
+                </form>
               </div>
-              <div className="overlay-panel overlay-right">
-                <h1>Hello, Friend!</h1>
-                <p>Enter your personal details and start journey with us</p>
-                <button className="ghost" id="signUp" onClick={Changestyle}>
-                  Sign Up
-                </button>
+              <div className="overlay-container">
+                <div className="overlay">
+                  <div className="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>
+                      To keep connected with us please login with your personal
+                      info
+                    </p>
+                    <button className="ghost" id="signIn" onClick={Changestyle}>
+                      Sign In
+                    </button>
+                  </div>
+                  <div className="overlay-panel overlay-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button className="ghost" id="signUp" onClick={Changestyle}>
+                      Sign Up
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          </>
         </div>
-      </>
-    </div>
+      )}
+    </>
   );
 }
